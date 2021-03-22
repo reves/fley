3822a37b4ad05ae63a7fcc80d67d3c686eb300be
Constantin Miron <mc.reves@gmail.com>
2021-03-22 04:12:51 +0200

    i18n:features implementation

--- i18n.js
+++ i18n.js
@@ -48,9 +48,23 @@ const pluralRules = [
 ]
 
 
-/**
- * State
- */
+// TODO:
+
+// Refactor spaghetti code
+
+// Data models iterpolation e.g. https://www.i18next.com/translation-function/interpolation#working-with-data-models
+
+// Date
+// Currency
+
+// Treat not found key error
+
+// Move pluralization rule to the locale file?
+
+// Use something like <lang key=”greeting”/> buit-in ley component, that listens to language state.
+// Lazy loading
+
+
 let locales = {}
 let fallback = null
 let locale = null
@@ -93,21 +107,61 @@ i18n.set = (code) => {
 
 i18n.getCode = _ => code
 
-// TODO: get a list of code => Language name
+i18n.getLocales = _ => {
+    const list = []
+    for (const key in locales) list.push([ key, (locales[key]['_name'] || '') ])
+    return list
+}
 
-i18n.t = (key, sub) => {
+i18n.t = (key, sub = null) => {
 
-    const value = getValue(key, locale) || getValue(key, fallback)
+    const parentKey = key.replace(/\.[^\.]*$/, '')
+    const fallbackKey = parentKey ? parentKey + '._' : ''
 
-    if (sub == null) return value
-    if (typeof sub === 'string') return value.replace('{n}', sub)
-    if (typeof sub === 'number') return value.replace(
-        /{n}(?:(\s*)\(([^)]+\|[^)]+)\))?/g, 
-        (_, $1, $2) => sub + ($1 || '') + ($2 ? $2.split('|', 10)[rule(sub)].trim() : '')
+    let value = getValue(key, locale) || 
+        getValue(fallbackKey, locale) ||
+        getValue(key, fallback) || 
+        getValue(fallbackKey, fallback)
+
+    if (value == null) {
+        console.error('Not found transaltion for the "' + key + '" key in the "' + code + '" locale.')
+        return ''
+    }
+
+    // Resolve "includes"
+    value = value.replace(
+        /\@\{([^\}]*)\}/g,
+        (_, $1) => $1.charAt(0) === '.' ? i18n.t(parentKey + $1) : i18n.t($1)
     )
 
-    // TODO: array indexes match bidings {0}... - same rules as above
-    // TODO: object values - same rules as for primitives
+    if (sub == null) return value
+    if (typeof sub === 'string') return value.replace(/\{s\}/g, sub)
+    if (typeof sub === 'number') return value.replace(
+        /\{n\}(?:(\s*)\(([^\)]*\|[^\)]*)\))?|\(([^\)]*\|[^\)]*)\)/g,
+        (_, $1, $2, $3) => $3 ? $3.replace(/\{n\}/g, sub).split('|', 10)[rule(Math.abs(sub))].trim() : sub + ($1 || '') + ($2 ? $2.split('|', 10)[rule(Math.abs(sub))].trim() : '')
+    )
+
+    if (sub instanceof Array) { /////////////// redo
+        sub.forEach((v,i) => {
+            if (v == null) return value = value.replace(new RegExp('\{' + i + '\}', 'g'), '')
+            if (typeof v === 'string') return value = value.replace(new RegExp('\{' + i + '\}', 'g'), v)
+            if (typeof v === 'number') return value = value.replace(
+                new RegExp('\\{' + i + '\\}(?:(\\s*)\\(([^\\)]*\\|[^\\)]*)\\))?', 'g'),
+                (_, $1, $2) => v + ($1 || '') + ($2 ? $2.split('|', 10)[rule(Math.abs(v))].trim() : '')
+            )
+        })
+
+        return value
+    } ///////////////////
+
+    for (const k in sub) { //////////////// redo
+        if (sub[k] == null) {value = value.replace(new RegExp('\{' + k + '\}', 'g'), ''); continue}
+        if (typeof sub[k] === 'string') {value = value.replace(new RegExp('\{' + k + '\}', 'g'), sub[k]); continue}
+        if (typeof sub[k]=== 'number') {value = value.replace(
+            new RegExp('\\{' + k + '\\}(?:(\\s*)\\(([^\\)]*\\|[^\\)]*)\\))?', 'g'),
+            (_, $1, $2) => sub[k] + ($1 || '') + ($2 ? $2.split('|', 10)[rule(Math.abs(sub[k]))].trim() : '')
+        ); continue}
+    } ///////////////////
 
     return value
 }
@@ -121,7 +175,7 @@ function getRule(code) {
 }
 
 function getValue(key, locale) {
-    return key.split('.').reduce((o, i) => o[i], locale)
+    return key ? key.split('.').reduce((o, i) => o[i], locale) : null
 }
 
 export default i18n
